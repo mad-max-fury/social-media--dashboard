c64b50b9821b7fa87301ec836362e8c946427253
mad-max-fury <chrisnonso8@gmail.com>
2020-04-24 12:15:13 +0100

    Completed responsiveness

--- funct.js
+++ funct.js
@@ -3,10 +3,22 @@ let toggleOff = document.getElementById('fa-toggle-off');
 let header = document.getElementById('First');
 let tail = document.getElementById('innerfirst');
 let all = document.getElementsByClassName('asocial');
-// console.log(all);
+let gray = document.getElementsByClassName('gray-text');
 
 
-toggleOff.style.display = 'none'
+toggleOff.style.display = 'none';
+
+for( each = 0; each < all.length; each++){
+    all[each].addEventListener('mouseover', function(event){
+        console.log('Akuko');
+        event.target.style.backgroundColor = "#f7f7f7";
+    
+        setTimeout(function() {
+            event.target.style.backgroundColor = "#f0f3fa";
+          }, 500);
+        
+    }, false);
+}
 
 
 toggleOn.addEventListener('click', function(){
@@ -20,24 +32,34 @@ toggleOn.addEventListener('click', function(){
 
     for( each = 0; each < all.length; each++){
         all[each].style.backgroundColor = '#252B43';
-        all[each].style.color = 'white'
-        
+        all[each].style.color = 'white';
+
+
+        all[each].addEventListener('mouseover', function(event){
+            console.log('Akuko');
+            event.target.style.backgroundColor = "#434447";
+    
+            setTimeout(function() {
+                event.target.style.backgroundColor = "#252B43";
+              }, 500);
+            
+        }, false)
         
     }
-
+   
 })
 
 toggleOff.addEventListener('click', function(){
-    // console.log('clicked');
-    document.body.style.backgroundColor = '#ffffff'
-    header.style.backgroundColor = '#f8f9fe'
-    toggleOff.style.display = 'none'
-    toggleOn.style.display = 'flex'
-    tail.style.color = 'black'
-    for( each = 0; each < all.length; each++){
-        all[each].style.backgroundColor = '#f5f5f5'
-        all[each].style.color = 'black'
-        
-        
-    }
+        // console.log('clicked');
+        document.body.style.backgroundColor = '#ffffff'
+        header.style.backgroundColor = '#f8f9fe'
+        toggleOff.style.display = 'none'
+        toggleOn.style.display = 'flex'
+        tail.style.color = 'black'
+        for( each = 0; each < all.length; each++){
+            all[each].style.backgroundColor = '#f5f5f5'
+            all[each].style.color = 'black';
+
+        }
 })
+
